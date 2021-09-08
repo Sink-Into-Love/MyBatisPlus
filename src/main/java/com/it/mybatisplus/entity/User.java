58e7de99d2fad9869292cd8415643e1d8988b171
zph <1305353123@qq.com>
2021-09-09 00:09:57 +0800

    基础的CRUD

--- src/main/java/com/it/mybatisplus/entity/User.java
+++ src/main/java/com/it/mybatisplus/entity/User.java
@@ -10,17 +10,25 @@ public class User implements Serializable {
     private static final long serialVersionUID = 133068166872292620L;
     /**
      * 主键ID
+     * AUTO(0) 自动增长,    NONE(1) 没有主键,    INPUT(2) 手动输入,
+     * ASSIGN_ID(3) 实体类用Long/String 表列属性使用bigint/varchar(50),   ASSIGN_UUID(4) 实体类用String 表列属性使用varchar(50),
+     * Deprecated
+     * ID_WORKER(3) 实体类用Long 表列属性使用bigint,   ID_WORKER_STR(3) 实体类用String 表列属性使用varchar(50),
+     * UUID(4) 实体类用String 表列属性使用varchar(50),
      */
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
+
     /**
      * 姓名
      */
     private String name;
+
     /**
      * 年龄
      */
     private Integer age;
+
     /**
      * 邮箱
      */
